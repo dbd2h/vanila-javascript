2b97a42ca70a1be3f76f4ff73fb515cccd968ee7
dbd2h <kangminchang1@gmail.com>
2022-01-31 03:43:51 +0900

    todo update

--- momentum-js/todo.js
+++ momentum-js/todo.js
@@ -1,7 +1,7 @@
 const $todoButton = document.querySelector(".momentum-footer__todolist");
 const $newTodo = document.querySelector(".todoscreen-content__button");
 const $todoscreen = document.querySelector(".todoscreen");
-const $todoinput = document.querySelector(".todoscreen-content>form>input");
+const $todoinput = document.querySelector(".todoscreen-content>input");
 const $checklists = document.querySelector(".checklists");
 const checklists = [];
 
@@ -12,15 +12,37 @@ $todoButton.addEventListener("click", () => {
   }
   $todoscreen.classList.toggle("hidden");
 });
+
 $newTodo.addEventListener("click", () => {
   $newTodo.classList.toggle("hidden");
   $todoinput.classList.toggle("hidden");
 });
-$todoinput.addEventListener("submit", (event) => {
-  event.preventDefault();
+
+$todoinput.addEventListener("keypress", (event) => {
   const content = event.target.value;
-  if (content == "") {
+  if (content == "" || event.key !== "Enter") {
     return;
   }
+
   const $div = document.createElement("div");
+  const $checkBox = document.createElement("div");
+  const $checkIcon = document.createElement("i");
+  const $checkEllipsis = document.createElement("i");
+  const $checkSpan = document.createElement("span");
+  $checkSpan.innerHTML = content;
+  $checkEllipsis.setAttribute("class", "fas fa-ellipsis-h");
+  $checkBox.append($checkIcon);
+  $div.append($checkBox);
+  $div.append($checkSpan);
+  $div.append($checkEllipsis);
+  $div.classList.add("todoCheckList");
+  $checklists.append($div);
+  $checklists.classList.remove("hidden");
+  document.querySelector(".todoscreen-content>span").classList.add("hidden");
+  document
+    .querySelector(".todoscreen-content>.todoscreen-content__switch")
+    .classList.add("hidden");
+  document
+    .querySelector(".todoscreen-content>.todoscreen-content__button")
+    .classList.add("hidden");
 });
